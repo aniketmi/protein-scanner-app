d29942248bf8af0b358827b2cb830492662385e2
Aniket Mitra <aniketm.87@gmail.com>
2025-07-01 13:36:31 -0400

    Update next.config.js
    updated config to support workers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Enable PWA features
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
